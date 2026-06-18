8662c6ed9329ffe990be3b292018688e5c50e0ff
Mohd Khushnihal Ansari <ansari.khushnihal@gmail.com>
2026-06-18 15:51:24 +0530

    Added Privacy Policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { ItTraining } from './pages/it-training/it-training';
 import { OurTeam } from './pages/our-team/our-team';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 
 export const routes: Routes = [
   { path: '', component: Home },
@@ -9,6 +10,7 @@ export const routes: Routes = [
   // { path: 'products', component: Home },
   // { path: 'features', component: Home },
   // { path: 'contact', component: Home },
+  { path: 'privacy-policy', component: PrivacyPolicy },
   { path: 'it-training', component: ItTraining },
   { path: 'our-team', component: OurTeam },
   { path: '**', redirectTo: '' },
